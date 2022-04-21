73f8394fc8e93137b361d9b1074e81cc621d2c79
Radek Křikava <krikavar.03@spst.cz>
2022-04-21 12:26:07 +0200

    Přidejte soubory projektu.

---
+++ testOOP/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace testOOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_LEFT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button_LEFT
+            // 
+            this.button_LEFT.Location = new System.Drawing.Point(320, 33);
+            this.button_LEFT.Name = "button_LEFT";
+            this.button_LEFT.Size = new System.Drawing.Size(75, 23);
+            this.button_LEFT.TabIndex = 1;
+            this.button_LEFT.Text = "Klikni";
+            this.button_LEFT.UseVisualStyleBackColor = true;
+            this.button_LEFT.Click += new System.EventHandler(this.button_LEFT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_LEFT);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_LEFT;
+    }
+}
+
---
+++ testOOP/Herní postava.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testOOP
+{
+    class Herní_postava 
+    {
+        
+        private int level = 1;
+        protected string jmeno; 
+        public int pozY { get { return pozY; } set { pozY = 0; } }
+        public int pozX { get { return pozX; } set { pozX = 0; } }
+
+
+    }
+}
